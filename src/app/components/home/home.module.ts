bbbc7d993873449a975507444afbc5a6950bf5d9
Asu Software <antonio.popa.dev@gmail.com>
2023-02-10 17:13:52 +0200

    Added new sections

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,3 +1,4 @@
+import { FooterComponent } from './../../shared/footer/footer.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -9,7 +10,8 @@ import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
